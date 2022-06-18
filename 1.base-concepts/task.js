c1f7187353f83c3021c839b359a17406e14adb7a
Sportsmanzao4nick <atomserg@yandex.ru>
2022-06-18 12:30:35 +0500

    add numeric conversion

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -19,27 +19,35 @@ function solveEquation(a, b, c) {
 
 function calculateTotalMortgage(percent, contribution, amount, date) {
   'use strict' 
-  if (isNaN(percent)) {
+  let percentNumber = Number(percent);
+  let contributionNumber = Number(contribution);
+  let amountNumber = Number(amount);
+  let dateNowNumber = Number(new Date ());
+
+  if (isNaN(percentNumber)) {
     alert(`Параметр "Процентная ставка" содержит неправильное значение "${percent}"`);
     return;
   }
-  if (isNaN(contribution)) {
-    alert(`Параметр "Процентная ставка" содержит неправильное значение "${contribution}"`);
+  if (isNaN(contributionNumber)) {
+    alert(`Параметр "Начальный взнос" содержит неправильное значение "${contribution}"`);
     return;
   }
-  if (isNaN(amount)) {
-    alert(`Параметр "Процентная ставка" содержит неправильное значение "${amount}"`);
+  if (isNaN(amountNumber)) {
+    alert(`Параметр "Общая стоимость" содержит неправильное значение "${amount}"`);
     return;
   }
   if (isNaN(date)) {
-    alert(`Параметр "Процентная ставка" содержит неправильное значение "${date}"`);
+    alert(`Параметр "Дата" содержит неправильное значение "${date}"`);
     return;
-  }    
+  }
+  if (isNaN(dateNowNumber)) {
+    alert(`Параметр "Дата" содержит неправильное значение "${date}"`);
+    return;
+  }  
    
-  let s = Number(amount) - Number(contribution);
-  let now = Number(new Date ());  
-  let n = Math.trunc((date - now) / (1000 * 60 * 60 * 24 * 30));
-  let p = (Number(percent)/100)/12;
+  let s = amountNumber - contributionNumber;  
+  let n = Math.trunc((date - dateNowNumber) / (1000 * 60 * 60 * 24 * 30));
+  let p = percentNumber / 100 / 12;
   let mounthAmount = s * (p + (p / (((1+p)**n) - 1)));
   let totalAmount = (mounthAmount * n).toFixed(2);   
   return totalAmount;
