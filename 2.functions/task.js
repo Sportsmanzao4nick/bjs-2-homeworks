bebec0c536789ab9b3745abb1937facc953895ad
Sportsmanzao4nick <atomserg@yandex.ru>
2022-06-18 22:03:08 +0500

    function initial commit

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,31 +1,54 @@
 // Задание 1
 function getArrayParams(arr) {
-  let min, max, sum, avg;
-
-  // Ваш код
-
+  let min = arr[0];
+  let max = min;
+  let sum = 0;
+  let avg;  
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] > max) {
+      max = arr[i];
+    };
+    if (arr[i] < min) {
+      min = arr[i];
+    };
+    sum += arr[i];
+    avg = Number((sum / arr.length).toFixed(2));
+  }
   return { min: min, max: max, avg: avg };
 }
 
 // Задание 2
 function worker(arr) {
-  let sum;
-
-  // Ваш код
-
+  let sum = 0;
+  for (let i = 0; i < arr.length; i++){
+    sum += arr[i];
+  }
   return sum;
 }
 
 function makeWork(arrOfArr, func) {
-  let max;
-
-  // Ваш кода
-  // for ...
-  
+  let max = 0;  
+  for (let j = 0; j < arrOfArr.length; j++) {    
+    if (func(arrOfArr[j]) > max) {
+      max = func(arrOfArr[j]);      
+    }    
+  }
   return max;
 }
 
 // Задание 3
 function worker2(arr) {
-  // Ваш код
+  let min = arr[0];
+  let max = min;
+  let sum = 0;  
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] > max) {
+      max = arr[i];
+    };
+    if (arr[i] < min) {
+      min = arr[i];
+    };
+    sum = Math.abs(Math.abs(max) - Math.abs(min));
+  }
+  return sum;
 }
