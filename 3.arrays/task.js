a2d71f852c140f816c440dfe5b412bb06e6542c3
Sportsmanzao4nick <atomserg@yandex.ru>
2022-06-22 19:24:03 +0500

    initial commit

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,15 +1,12 @@
 function compareArrays(arr1, arr2) {
-  let result;
-
-  // Ваш код
-
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+  let result = arr1.every((value, idx) => value === arr2[idx]);
   return result; // boolean
 }
 
 function advancedFilter(arr) {
-  let resultArr;
-
-  // Ваш код
-
+  let resultArr = arr.filter((positive) => positive > 0).filter((freq) => freq % 3 === 0).map((multi) => multi * 10); 
   return resultArr; // array
 }
